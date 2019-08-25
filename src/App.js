ad341be52cac81f93421404847d3d1b3a3145b77
vedicastrology123 <mbchandru@gmail.com>
2019-08-25 18:21:22 +0530

    New Changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import { appConfig } from "./utils/constants";
+import { UserSession } from "blockstack";
+class App extends Component {
+  state = {
+    userSession: new UserSession({ appConfig })
+  };
+componentDidMount = async () => {
+    const { userSession } = this.state;
+if (!userSession.isUserSignedIn() && userSession.isSignInPending()) {
+      const userData = await userSession.handlePendingSignIn();
+if (!userData.username) {
+        throw new Error("This app requires a username");
+      }
+window.location = "/";
+    }
+  };
+handleSignIn = () => {
+    const { userSession } = this.state;
+    userSession.redirectToSignIn();
+  };
+handleSignOut = () => {
+    const { userSession } = this.state;
+    userSession.signUserOut();
+    window.location = "/";
+  };
+render() {
+    const { userSession } = this.state;
+    return (
+      <div className="App">
+        {userSession.isUserSignedIn() ? (
+          <button className="button" onClick={this.handleSignOut}>
+            <strong>Sign Out</strong>
+          </button>
+        ) : (
+          <button className="button" onClick={this.handleSignIn}>
+            <strong>Sign In</strong>
+          </button>
+        )}
+      </div>
+    );
+  }
 }
-
 export default App;
